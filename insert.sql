0d435f6ce784cdc0794f024a1b6147b9677b7db3
Ariunmunkh.e <ariunmunkh.e@digitalworks.mn>
2023-01-15 20:16:36 +0800

    add db file

---
+++ insert.sql
@@ -0,0 +1,159 @@
+
+
+INSERT INTO tbluser (userid, username, email, password) values (@userid, @username, @email, @password)
+ON DUPLICATE KEY
+UPDATE username = @username,
+         email    = @email,
+         password = @password;
+         
+insert into household
+  (householdid ,
+numberof ,
+name ,
+district ,
+section ,
+address ,
+phone ,
+coachid ,
+updatedby  )
+values
+  (@householdid ,
+@numberof ,
+@name ,
+@district ,
+@section ,
+@address ,
+@phone ,
+@coachid ,
+@updatedby) 
+on duplicate key update 
+numberof =@numberof,
+name=@name ,
+district=@district ,
+section=@section ,
+address=@address ,
+phone=@phone ,
+coachid=@coachid ,
+updated = current_timestamp,
+updatedby =@updatedby
+
+
+insert into householdmember
+(memberid,
+householdid,
+name,
+relative,
+birthdate,
+gender,
+istogether,
+updatedby )
+values
+(@memberid,
+@householdid,
+@name,
+@relative,
+@birthdate,
+@gender,
+@istogether,
+@updatedby) 
+on duplicate key update 
+householdid=@householdid,
+name=@name,
+relative=@relative,
+birthdate=@birthdate,
+gender=@gender,
+istogether=@istogether,
+updated=current_timestamp,
+updatedby=@updatedby
+
+SELECT 
+    householdid,
+    numberof,
+    name,
+    district,
+    section,
+    address,
+    phone,
+    coachid,
+    DATE_FORMAT(updated, '%Y-%m-%d %H:%i:%s') updated
+FROM
+    household
+
+
+
+    
+insert into district (districtid , name) values(	1, 'Багануур дүүрэг'),
+	(	4, 'Багахагай дүүрэг'),
+	(	7, 'Баянгол дүүрэг'),
+	(	10, 'Баянзүрх дүүрэг'),
+	(	13, 'Налайх дүүрэг'),
+	(	16, 'Сонгинохайрхан дүүрэг'),
+	(	19, 'Сүхбаатар дүүрэг'),
+	(	22, 'Хан-Уул дүүрэг'),
+	(	25, 'Чингэлтэй дүүрэг');
+
+insert into relationship (  name,relationshipid, ishead) values
+('ӨРХИЙН ТЭРГҮҮН', 	01,true),
+('ЭХНЭР/ НӨХӨР', 	02,false),
+('ХҮҮ/ ОХИН', 	03,false),
+('ЭЦЭГ/ ЭХ',	04,false),
+('АХ/ ЭГЧ/ ДҮҮ',	05,false),
+('ХАДАМ ЭЦЭГ/ ХАДАМ ЭХ',	06,false),
+('ХҮРГЭН/ БЭР',	07,false),
+('ӨВӨГ ЭЦЭГ/ ЭМЭГ ЭХ',	08,false),
+('АЧ/ ЗЭЭ/ ГУЧ',	09,false),
+('БУСАД ТӨРӨЛ ТӨРӨГСӨД',	10,false),
+('ХАМААРАЛГҮЙ',	11,false);
+
+insert into coach (coachid,name) values(1,'coach 01'),
+(2,'coach 02'),
+(3,'coach 03'),
+(4,'coach 04'),
+(5,'coach 05'),
+(6,'coach 06'),
+(7,'coach 07'),
+(8,'coach 08'),
+(9,'coach 09'),
+(10,'coach 10'),
+(11,'coach 11'),
+(12,'coach 12'),
+(13,'coach 13'),
+(14,'coach 14'),
+(15,'coach 15'),
+(16,'coach 16'),
+(17,'coach 17'),
+(18,'coach 18'),
+(19,'coach 19'),
+(20,'coach 20');
+
+select * from tbluser;
+
+insert into tbluser(
+email ,
+password ,
+userid , 
+username ,
+roleid ,
+coachid )values('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',101,'coach01',3,1),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',102,'coach02',3,2),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',103,'coach03',3,3),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',104,'coach04',3,4),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',105,'coach05',3,5),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',106,'coach06',3,6),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',107,'coach07',3,7),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',108,'coach08',3,8),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',109,'coach09',3,9),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',110,'coach10',3,10),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',111,'coach11',3,11),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',112,'coach12',3,12),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',113,'coach13',3,13),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',114,'coach14',3,14),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',115,'coach15',3,15),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',116,'coach16',3,16),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',117,'coach17',3,17),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',118,'coach18',3,18),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',119,'coach19',3,19),
+('coach@mail.mn','?-}?{?+??aE?Ka?"2{.?????Ud???',120,'coach20',3,20);
+
+
+
---
+++ table.sql
@@ -0,0 +1,368 @@
+create table tbluser(
+userid int not null, 
+username varchar(200) not null,
+email varchar(200) not null,
+password varchar(500) not null,
+roleid int,
+coachid int,
+updated timestamp default current_timestamp,
+updatedby int,
+PRIMARY KEY (userid)
+);
+create table household
+(
+householdid int not null, 
+status int default 0,
+numberof int,
+name varchar(200),
+districtid int,
+section int,
+address varchar(200),
+phone varchar(200),
+coachid int,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (householdid)
+);
+create table householdmember
+(
+memberid int not null, 
+householdid int,
+name varchar(200),
+relationshipid int,
+birthdate datetime,
+gender int,
+isparticipant bool,
+istogether bool,
+educationdegreeid int,
+employmentstatusid int,
+healthconditionid int,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (memberid),
+INDEX ind_householdmember_educationdegreeid (educationdegreeid),  
+CONSTRAINT fk_householdmember_educationdegreeid FOREIGN KEY (educationdegreeid) REFERENCES educationdegree(ID),
+INDEX ind_householdmember_employmentstatusid (employmentstatusid),  
+CONSTRAINT fk_householdmember_employmentstatusid FOREIGN KEY (employmentstatusid) REFERENCES employmentstatus(ID),
+INDEX ind_householdmember_healthconditionid (healthconditionid),  
+CONSTRAINT fk_householdmember_healthconditionid FOREIGN KEY (healthconditionid) REFERENCES healthcondition(ID)  
+);
+create table district
+(
+districtid int not null, 
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (districtid)
+);
+create table relationship
+(
+relationshipid int not null,
+name varchar(200),
+ishead bool,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (relationshipid)
+);
+create table coach
+(
+coachid int not null,
+name varchar(200),
+phone varchar(20),
+districtid int,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (coachid)
+);
+create table householdvisit
+(
+visitid int not null,
+householdid int,
+visitdate datetime,
+memberid int,
+coachid int,
+note varchar(2000),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (visitid),
+INDEX ind_householdvisit_memberid (memberid),  
+CONSTRAINT fk_householdvisit_memberid FOREIGN KEY (memberid) REFERENCES householdmember(memberid),
+INDEX ind_householdvisit_coachid (coachid),  
+CONSTRAINT fk_householdvisit_coachid FOREIGN KEY (coachid) REFERENCES coach(coachid)
+);
+create table meetingattendance
+(
+entryid int not null,
+householdid int,
+meetingdate datetime,
+isjoin bool,
+quantity int,
+amount decimal,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (entryid),
+INDEX ind_meetingattendance_householdid (householdid),  
+CONSTRAINT fk_meetingattendance_householdid FOREIGN KEY (householdid) REFERENCES household(householdid)
+);
+create table loan
+(
+entryid int not null,
+householdid int,
+loandate datetime,
+amount decimal,
+loanpurposeid int,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (entryid),
+INDEX ind_loan_householdid (householdid),  
+CONSTRAINT fk_loan_householdid FOREIGN KEY (householdid) REFERENCES household(householdid),
+INDEX ind_loan_loanpurposeid (loanpurposeid),  
+CONSTRAINT fk_loan_loanpurposeid FOREIGN KEY (loanpurposeid) REFERENCES loanpurpose(id)
+);
+create table loanrepayment
+(
+entryid int not null,
+householdid int,
+repaymentdate datetime,
+amount decimal,
+balance decimal,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (entryid),
+INDEX ind_loanrepayment_householdid (householdid),  
+CONSTRAINT fk_loanrepayment_householdid FOREIGN KEY (householdid) REFERENCES household(householdid)
+);
+create table training
+(
+entryid int not null,
+householdid int,
+trainingdate datetime,
+trainingtypeid int,
+trainingandactivityid int,
+organizationid int,
+duration decimal,
+isjoin bool,
+memberid int,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (entryid),
+INDEX ind_training_householdid (householdid),  
+CONSTRAINT fk_training_householdid FOREIGN KEY (householdid) REFERENCES household(householdid),
+INDEX ind_training_memberid (memberid),  
+CONSTRAINT fk_training_memberid FOREIGN KEY (memberid) REFERENCES householdmember(memberid),
+INDEX ind_training_trainingtypeid (trainingtypeid),  
+CONSTRAINT fk_training_trainingtypeid FOREIGN KEY (trainingtypeid) REFERENCES trainingtype(id),
+INDEX ind_training_trainingandactivityid (trainingandactivityid),  
+CONSTRAINT fk_training_trainingandactivityid FOREIGN KEY (trainingandactivityid) REFERENCES trainingandactivity(id),
+INDEX ind_training_organizationid (organizationid),  
+CONSTRAINT fk_training_organizationid FOREIGN KEY (organizationid) REFERENCES organization(id)
+);
+create table improvement
+(
+entryid int not null,
+householdid int,
+plandate datetime,
+selectedfarm varchar(200),
+subbranchid int,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (entryid),
+INDEX ind_improvement_householdid (householdid),  
+CONSTRAINT fk_improvement_householdid FOREIGN KEY (householdid) REFERENCES household(householdid),
+INDEX ind_improvement_subbranchid (subbranchid),  
+CONSTRAINT fk_improvement_subbranchid FOREIGN KEY (subbranchid) REFERENCES subbranch(id)
+);
+create table investment
+(
+entryid int not null,
+householdid int,
+investmentdate datetime,
+assetreceivedtypeid int,
+assetreceivedid int,
+quantity decimal,
+unitprice decimal,
+totalprice decimal,
+note varchar(2000),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (entryid),
+INDEX ind_investment_householdid (householdid),  
+CONSTRAINT fk_investment_householdid FOREIGN KEY (householdid) REFERENCES household(householdid),
+INDEX ind_improvement_assetreceivedtypeid (assetreceivedtypeid),  
+CONSTRAINT fk_investment_assetreceivedtypeid FOREIGN KEY (assetreceivedtypeid) REFERENCES assetreceivedtype(id),
+INDEX ind_improvement_assetreceivedid (assetreceivedid),  
+CONSTRAINT fk_investment_assetreceivedid FOREIGN KEY (assetreceivedid) REFERENCES assetreceived(id)
+);
+create table othersupport
+(
+entryid int not null,
+householdid int,
+supportdate datetime,
+supportreceivedtypeid int,
+name varchar(200),
+quantity  decimal,
+unitprice  decimal,
+totalprice decimal,
+sponsoringorganizationid int,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (entryid),
+INDEX ind_othersupport_householdid (householdid),  
+CONSTRAINT fk_othersupport_householdid FOREIGN KEY (householdid) 
+REFERENCES household(householdid),
+INDEX ind_othersupport_supportreceivedtypeid (supportreceivedtypeid),  
+CONSTRAINT fk_othersupport_supportreceivedtypeid FOREIGN KEY (supportreceivedtypeid) REFERENCES supportreceivedtype(id),
+INDEX ind_othersupport_sponsoringorganizationid (sponsoringorganizationid),  
+CONSTRAINT fk_othersupport_sponsoringorganizationid FOREIGN KEY (sponsoringorganizationid) REFERENCES sponsoringorganization(id)
+);
+create table mediatedactivity
+(
+entryid int not null,
+householdid int,
+mediateddate datetime,
+mediatedservicetypeid int,
+intermediaryorganizationid int,
+proxyserviceid int,
+memberid int,
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (entryid),
+INDEX ind_mediatedactivity_householdid (householdid),  
+CONSTRAINT fk_mediatedactivity_householdid FOREIGN KEY (householdid) REFERENCES household(householdid),
+INDEX ind_mediatedactivity_memberid (memberid),  
+CONSTRAINT fk_mediatedactivity_memberid FOREIGN KEY (memberid) REFERENCES householdmember(memberid),
+INDEX ind_mediatedactivity_mediatedservicetypeid (mediatedservicetypeid),  
+CONSTRAINT fk_mediatedactivity_mediatedservicetypeid FOREIGN KEY (mediatedservicetypeid) REFERENCES mediatedservicetype(id),
+INDEX ind_mediatedactivity_intermediaryorganizationid (intermediaryorganizationid),  
+CONSTRAINT fk_mediatedactivity_intermediaryorganizationid FOREIGN KEY (intermediaryorganizationid) REFERENCES intermediaryorganization(id),
+INDEX ind_mediatedactivity_proxyserviceid (proxyserviceid),  
+CONSTRAINT fk_mediatedactivity_proxyserviceid FOREIGN KEY (proxyserviceid) REFERENCES proxyservice(id)
+);
+create table householdstatus
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table educationdegree
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table employmentstatus
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table healthcondition
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table loanpurpose
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table trainingtype
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table trainingandactivity
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table organization
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table subbranch
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table assetreceivedtype
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table assetreceived
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table supportreceivedtype
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table sponsoringorganization
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table mediatedservicetype
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table intermediaryorganization
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
+create table proxyservice
+(
+id int not null,
+name varchar(200),
+updated timestamp default current_timestamp,
+updatedby int,  
+PRIMARY KEY (id)
+);
